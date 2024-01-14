2dc34a04c5782a30734df0d70f847f7a46c105e2
Sskrill <154072620+Sskrill@users.noreply.github.com>
2024-01-14 12:19:05 +0600

    Update errors.go

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,6 +3,6 @@ package domain
 import "errors"
 
 var (
-	ErrBookNotFound        = errors.New("book not found")
+	ErrEmplNotFound        = errors.New("employee not found")
 	ErrRefreshTokenExpired = errors.New("refresh token expired")
 )
